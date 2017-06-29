544b15c28d418a9ec6a9fc23c647684fc166ef73
Elias Myronidis <elias.myronidis@hotmail.com>
2017-06-29 13:50:14 +0300

    • Added strategy pattern UML diagram

--- src/strategy_pattern/models/Troll.java
+++ src/strategy_pattern/models/Troll.java
@@ -6,7 +6,7 @@ import strategy_pattern.behaviors.AxeBehavior;
  * Created by eliamyro on 29/6/17.
  */
 public class Troll extends Character {
-    
+
     public Troll() {
         weaponBehavior = new AxeBehavior();
     }
